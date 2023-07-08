c564ae41a56cb2d8803f4a208b6b1a34dd2d87c9
blanca34 <carlos1110fuentes@gmail.com>
2023-07-08 01:57:55 -0600

    :rocket:Patron de distribucion v2 e implementacion de navegacion

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from './src/Views/SplashScreen';
+import AppNavigator from './src/Navigation/appNavigator';
+const App = () => {
+  const [isSplashFinished, setIsSplashFinished] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSplashFinished(true);
+    }, 2000);
+  }, []);
+
+  if (!isSplashFinished) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
